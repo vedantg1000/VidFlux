116b853e1ad20926a58478e971dcb886df510299
vedantg1000 <69249835+vedantg1000@users.noreply.github.com>
2026-02-09 00:55:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+const data = {
+
+    show1: [
+        { title: "Episode 1", link: "https://drive.google.com/file/d/1D2v0mvKWDKHM1YrDw0Q8MdTYgn3-Gl14/view?usp=drive_link" },
+        { title: "Episode 2", link: "https://drive.google.com/file/d/1lpcJPu_Uhgi8YF0DPeFhINg6456ey63o/view?usp=drive_link" },
+        { title: "Episode 3", link: "https://drive.google.com/file/d/1s7QKuIverm2iFnHlQUEmsp8d9JGXyJRv/view?usp=drive_link" },
+        { title: "Episode 4", link: "https://drive.google.com/file/d/1GNGSyVDcRuAopev91RIOLhdlZjf9IWnS/view?usp=drive_link" },
+        { title: "Episode 5", link: "https://drive.google.com/file/d/18SuQCf6JmyhOQGbqPKaNFB1H--EYbKNU/view?usp=drive_link" },
+        { title: "Episode 6", link: "https://drive.google.com/file/d/1iZ_ibXUK1RwmnNsjK98Waljd2DT7PooL/view?usp=drive_link" },
+        { title: "Episode 7", link: "https://drive.google.com/file/d/1Taz1-sgdb6vZglzFq6r9nveWWfZEM4AZ/view?usp=drive_link" },
+        { title: "Episode 8", link: "https://drive.google.com/file/d/1WceXihkBQwBHNrcgtsllEHYQgrBIf_Ab/view?usp=drive_link" },
+        { title: "Episode 9", link: "https://drive.google.com/file/d/17g-nROljwmnhM1AY62mB1nPHCaz0CGPr/view?usp=drive_link" },
+        { title: "Episode 10", link: "https://drive.google.com/file/d/12dpXW2bgm5Sq50Lj8Wza2C036Jw22AeE/view?usp=drive_link" }
+    ],
+
+    show2: [
+        { title: "Episode 1", link: "https://drive.google.com/file/d/1zFUImnhGo9FQ4vHgpdyJ0XF5mTgKUOc5/view?usp=sharing" },
+        { title: "Episode 2", link: "https://drive.google.com/file/d/1Aw6q1S0QbYWju6f9hEDuTyGuMKgQp-Ch/view?usp=drive_link" },
+        { title: "Episode 3", link: "https://drive.google.com/file/d/1naQ-Bzy1vql1ZbArSnI26Um-uGR1JFSL/view?usp=drive_link" },
+        { title: "Episode 4", link: "https://drive.google.com/file/d/14RXz1XMOVnnyREHIRpxQ5ESVH20P4bAi/view?usp=drive_link" },
+        { title: "Episode 5", link: "https://drive.google.com/file/d/1btAzx92AIuS7tozodEwYFVq6seba7I0W/view?usp=drive_link" },
+        { title: "Episode 6", link: "https://drive.google.com/file/d/1JS3ADtURau1bdZuPvlAbK7dCfH7ZK863/view?usp=sharing" },
+        { title: "Episode 7", link: "https://drive.google.com/file/d/1by7KA4SZN1tzP_sovOSGAsjrGKbV7Uo6/view?usp=drive_link" },
+        { title: "Episode 8", link: "https://drive.google.com/file/d/1Cli4V-siYxAi_miTrJPyLcR5rdtfrhGM/view?usp=drive_link" },
+        { title: "Episode 9", link: "https://drive.google.com/file/d/16wa4syfmwuVDmU_8SZCNOxSH3orlg2UX/view?usp=drive_link" },
+        { title: "Episode 10", link: "https://drive.google.com/file/d/1s7knwsTChuKTil5jVQpejubxZkKbnlpO/view?usp=drive_link" }
+    ],
+
+    show3: [
+        { title: "Episode 1", link: "https://drive.google.com/file/d/1ChOP0zx_ZqHM2fmsTQyp0M6w1W_Z7RCC/view?usp=drive_link" },
+        { title: "Episode 2", link: "https://drive.google.com/file/d/1W98JWAtlRxVyWf4EiAPNV3n4tx8_jgc_/view?usp=drive_link" },
+        { title: "Episode 3", link: "https://drive.google.com/file/d/1euvhYaoKVYEDKHYJhRTB5qvIlHxu9m8s/view?usp=drive_link" },
+        { title: "Episode 4", link: "https://drive.google.com/file/d/1SK7r-HSw2e9_YXeEMGBZfVuh7fZxiQPW/view?usp=drive_link" },
+        { title: "Episode 5", link: "https://drive.google.com/file/d/1y2b8GaQW_nAz8xS1qe77WaElL7fB_l5N/view?usp=drive_link" },
+        { title: "Episode 6", link: "https://drive.google.com/file/d/1iIbXP-cowOn9x9pWFJJLCyn9KSRSaXdD/view?usp=drive_link" },
+        { title: "Episode 7", link: "https://drive.google.com/file/d/1Nalik0-EzQDj2v9JcKOWD52-Ad-F97OD/view?usp=drive_link" },
+        { title: "Episode 8", link: "https://drive.google.com/file/d/1O6g1EaEyJcKwk2I6sT-1DmPtAXR1F3t3/view?usp=drive_link" },
+        { title: "Episode 9", link: "https://drive.google.com/file/d/1dXSpr604gwHG8m6_6qw4GaYYv5NbBIEz/view?usp=drive_link" },
+        { title: "Episode 10", link: "https://drive.google.com/file/d/1LoJOplSGVM-paqQtVYoaNOZM2FaSQ2eD/view?usp=drive_link" }
+    ],
+
+    show4: [
+        { title: "Episode 1", link: "" },
+        { title: "Episode 2", link: "" },
+        { title: "Episode 3", link: "" },
+        { title: "Episode 4", link: "" },
+        { title: "Episode 5", link: "" },
+        { title: "Episode 6", link: "" },
+        { title: "Episode 7", link: "" },
+        { title: "Episode 8", link: "" },
+        { title: "Episode 9", link: "" },
+        { title: "Episode 10", link: "" }
+    ],
+
+    show5: [
+        { title: "Episode 1", link: "" },
+        { title: "Episode 2", link: "" },
+        { title: "Episode 3", link: "" },
+        { title: "Episode 4", link: "" },
+        { title: "Episode 5", link: "" },
+        { title: "Episode 6", link: "" },
+        { title: "Episode 7", link: "" },
+        { title: "Episode 8", link: "" },
+        { title: "Episode 9", link: "" },
+        { title: "Episode 10", link: "" }
+    ],
+
+    show6: [
+        { title: "Episode 1", link: "" },
+        { title: "Episode 2", link: "" },
+        { title: "Episode 3", link: "" },
+        { title: "Episode 4", link: "" },
+        { title: "Episode 5", link: "" },
+        { title: "Episode 6", link: "" },
+        { title: "Episode 7", link: "" },
+        { title: "Episode 8", link: "" },
+        { title: "Episode 9", link: "" },
+        { title: "Episode 10", link: "" }
+    ],
+
+    show7: [
+        { title: "Episode 1", link: "" },
+        { title: "Episode 2", link: "" },
+        { title: "Episode 3", link: "" },
+        { title: "Episode 4", link: "" },
+        { title: "Episode 5", link: "" },
+        { title: "Episode 6", link: "" },
+        { title: "Episode 7", link: "" },
+        { title: "Episode 8", link: "" },
+        { title: "Episode 9", link: "" },
+        { title: "Episode 10", link: "" }
+    ],
+
+    show8: [
+        { title: "Episode 1", link: "" },
+        { title: "Episode 2", link: "" },
+        { title: "Episode 3", link: "" },
+        { title: "Episode 4", link: "" },
+        { title: "Episode 5", link: "" },
+        { title: "Episode 6", link: "" },
+        { title: "Episode 7", link: "" },
+        { title: "Episode 8", link: "" },
+        { title: "Episode 9", link: "" },
+        { title: "Episode 10", link: "" }
+    ],
+
+    show9: [
+        { title: "Episode 1", link: "" },
+        { title: "Episode 2", link: "" },
+        { title: "Episode 3", link: "" },
+        { title: "Episode 4", link: "" },
+        { title: "Episode 5", link: "" },
+        { title: "Episode 6", link: "" },
+        { title: "Episode 7", link: "" },
+        { title: "Episode 8", link: "" },
+        { title: "Episode 9", link: "" },
+        { title: "Episode 10", link: "" }
+    ],
+
+    show10: [
+        { title: "Episode 1", link: "" },
+        { title: "Episode 2", link: "" },
+        { title: "Episode 3", link: "" },
+        { title: "Episode 4", link: "" },
+        { title: "Episode 5", link: "" },
+        { title: "Episode 6", link: "" },
+        { title: "Episode 7", link: "" },
+        { title: "Episode 8", link: "" },
+        { title: "Episode 9", link: "" },
+        { title: "Episode 10", link: "" }
+    ]
+};
+
+function openShow(show) {
+    localStorage.setItem("show", show);
+    window.location.href = "episodes.html";
+}
+
+
+if (window.location.pathname.includes("episodes.html")) {
+    const show = localStorage.getItem("show");
+    const container = document.getElementById("episodes-container");
+
+    data[show].forEach(ep => {
+        const div = document.createElement("div");
+        div.className = "episode";
+        div.innerText = ep.title;
+        div.onclick = () => {
+            const id = ep.link.split("/d/")[1].split("/")[0];
+            localStorage.setItem("videoId", id);
+            window.location.href = "player.html";
+        };
+
+        container.appendChild(div);
+    });
+
+}
